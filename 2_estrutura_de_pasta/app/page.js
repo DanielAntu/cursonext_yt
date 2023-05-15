92ed60e494420f28d2ed234c2dcdf15f5abdffdb
DanielAntunes <antunesdaniel083@gmail.com>
2023-05-15 14:45:06 -0300

    Atualização

---
+++ 2_estrutura_de_pasta/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+    return (
+        <main className={styles.main}>
+            <h2>Hello Word next js</h2>
+        </main>
+    );
+}
